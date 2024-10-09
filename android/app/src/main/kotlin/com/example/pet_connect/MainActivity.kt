57e4204a4b6ef31bcd45b26fea1781c6e27f2a8a
Yazdan Haider <yazdanhaider007@gmail.com>
2024-10-09 13:05:52 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/pet_connect/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pet_connect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pet_connect/router/app_router.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Pet Connect+',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routerConfig: router,
+    );
+  }
+}
